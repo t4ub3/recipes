612509a88ea9e661deaef7dcb4cad48491434a97
Jonas <jn.jonasneumann@googlemail.com>
2017-10-07 19:30:50 +0200

    add database connection and measures api

---
+++ api/controllers/MeasuresController.js
@@ -0,0 +1,11 @@
+/**
+ * MeasuresController
+ *
+ * @description :: Server-side logic for managing measures
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Measures.js
@@ -0,0 +1,16 @@
+/**
+ * Measures.js
+ *
+ * @description :: Measures in which ingredients can be measured
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string'
+    }
+  }
+};
+
